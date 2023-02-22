068272a67ee101e1b9db501e8e6a479668f536d0
Brian Yee <beeyee0401@gmail.com>
2023-02-22 15:06:47 -0600

    remove card color. Add brackets

--- src/main/java/MasteryChallenge/patches/RunHistoryScreenPatch.java
+++ src/main/java/MasteryChallenge/patches/RunHistoryScreenPatch.java
@@ -8,6 +8,7 @@ import com.evacipated.cardcrawl.modthespire.lib.ByRef;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.screens.runHistory.RunHistoryScreen;
 import com.megacrit.cardcrawl.screens.runHistory.TinyCard;
 import com.megacrit.cardcrawl.screens.stats.RunData;
@@ -27,17 +28,30 @@ public class RunHistoryScreenPatch {
     }
 
     @SpirePatch2(clz = TinyCard.class, method = "render")
-    public static class ShowCardMasteriesInHistoryColorPatch {
+    public static class ShowCardMasteriesInHistoryPatch {
         @SpireInsertPatch(
-                rloc = 80-67,
-                localvars = {"textColor"}
+                rloc = 74-67,
+                localvars = {"text"}
         )
-        public static void setCurrentRun(TinyCard __instance, SpriteBatch sb, @ByRef Color[] textColor) {
+        public static void setCurrentRun(TinyCard __instance, SpriteBatch sb, @ByRef String[] text) {
             if (MasteryChallenge.cardAndRunMap.getOrDefault(__instance.card.cardID, "").equals(currentRunTimestamp)){
-                if (!__instance.hb.hovered){
-                    textColor[0] = Color.CYAN;
-                }
+                text[0] = "[" + text[0] + "]";
             }
         }
     }
+
+//    @SpirePatch2(clz = TinyCard.class, method = "render")
+//    public static class ShowCardMasteriesInHistoryColorPatch {
+//        @SpireInsertPatch(
+//                rloc = 80-67,
+//                localvars = {"textColor"}
+//        )
+//        public static void setCurrentRun(TinyCard __instance, SpriteBatch sb, @ByRef Color[] textColor) {
+//            if (MasteryChallenge.cardAndRunMap.getOrDefault(__instance.card.cardID, "").equals(currentRunTimestamp)){
+//                if (!__instance.hb.hovered){
+//                    textColor[0] = Color.CYAN;
+//                }
+//            }
+//        }
+//    }
 }
